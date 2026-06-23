6b20f5e389028b020644ccfb777077dc64b6057a
PYongju <wns82978@naver.com>
2026-06-23 18:43:17 +0900

    서버 죽음 원인3

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -12,7 +12,7 @@ engine = create_engine(
     pool_pre_ping=True,
     pool_size=5,
     max_overflow=10,
-    pool_recycle=1800,
+    pool_recycle=180,
     pool_timeout=30,
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
